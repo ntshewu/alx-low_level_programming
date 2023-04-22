d885119b1aaeb25ce67d38c1e21d5f824f6438c4
ntshewu <ntshewu@gmail.com>
2023-04-21 19:17:07 -0700

    Positive anything is better than negative nothing

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char c;
+char d;
+c = 'a';
+d = 'A';
+while
+(c <= 'z') {
+putchar(c);
+c++;
+}
+while
+(d <= 'Z') {
+putchar(d);
+d++;
+}
+putchar('\n');
+return (0);
+}
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+printf("%d", a);
+putchar('\n');
+return (0);
+}
