5af70706824fe2f2f56ee02a2b237c5c75069c91
ntshewu <ntshewu@gmail.com>
2023-05-12 11:41:50 -0700

    Task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <main.h>
+/**
+ * main - print number and new line
+ * @argc: command line arg
+ * @argv: command line arg
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
