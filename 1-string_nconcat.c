1ce7abfe97d9364912ead88ee088783dc6f8bcf8
ntshewu <ntshewu@gmail.com>
2023-05-18 13:25:29 -0700

    Task1

---
+++ 1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: string one
+ * @s2: string two
+ *
+ ** Return: new space in memory
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *nstr, *empt;
+	unsigned int i, len, j;
+	unsigned int size;
+
+	len = 0;
+	empt = "";
+	if (s1 == NULL)
+		s1 = empt;
+	if (s2 == NULL)
+		s2 = empt;
+	while (s1[len] != '\0')
+		len++;
+	size = (len + n) * sizeof(*nstr);
+	nstr = malloc(size + 1);
+	if (nstr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size && s1[i] != '\0')
+	{
+		nstr[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (i < size && s2[j] != '\0')
+	{
+		nstr[i] = s2[j];
+		i++;
+		j++;
+	}
+	nstr[i] = '\0';
+	return (nstr);
+}
