457370f98492f8fd077b9a8cb63ef1231da6433c
ntshewu <ntshewu@gmail.com>
2023-05-18 13:47:19 -0700

    Task1

--- 1-string_nconcat.c
+++ 1-string_nconcat.c
@@ -2,44 +2,48 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - Concatenate two strings
+ * string_nconcat - concatenates two strings
  * @s1: string one
  * @s2: string two
- * @n: size of string added
+ * @n: bytes amount
  *
- * Return: pointer to new space in memory
+ * Return: pointer to newly allocated memory
+ * if fails status Null
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *nstr, *empt;
-	unsigned int i, len, j;
-	unsigned int size;
+	char *sout;
+	unsigned int ls1, ls2, lsout, i;
 
-	len = 0;
-	empt = "";
 	if (s1 == NULL)
-		s1 = empt;
+		s1 = "";
+
 	if (s2 == NULL)
-		s2 = empt;
-	while (s1[len] != '\0')
-		len++;
-	size = (len + n) * sizeof(*nstr);
-	nstr = malloc(size + 1);
-	if (nstr == NULL)
+		s2 = "";
+
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
+		;
+
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+		;
+
+	if (n > ls2)
+		n = ls2;
+
+	lsout = ls1 + n;
+
+	sout = malloc(lsout + 1);
+
+	if (sout == NULL)
 		return (NULL);
-	i = 0;
-	while (i < size && s1[i] != '\0')
-	{
-		nstr[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (i < size && s2[j] != '\0')
-	{
-		nstr[i] = s2[j];
-		i++;
-		j++;
-	}
-	nstr[i] = '\0';
-	return (nstr);
+
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+			sout[i] = s1[i];
+		else
+			sout[i] = s2[i - ls1];
+
+	sout[i] = '\0';
+
+	return (sout);
 }
