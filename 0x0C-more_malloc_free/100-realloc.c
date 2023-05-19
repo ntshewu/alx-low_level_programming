6df00158b66f41790759f83f8682b0f40bde74e6
ntshewu <ntshewu@gmail.com>
2023-05-19 13:52:03 -0700

    Task4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -7,7 +7,9 @@
  * @old_size: The size in bytes of the allocated space for ptr
  * @new_size: The size in bytes of the new memory block
  *
- * Return: pointer to the reallocated memory block
+ * Return: If new_size == old_size - ptr
+ *         If new_size == 0 and ptr is not NULL - NULL
+ *         Otherwise - a pointer to the reallocated memory block
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
