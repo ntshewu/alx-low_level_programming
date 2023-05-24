f700b179f0568bad7f51384f65142eed0de9a8e9
ntshewu <ntshewu@gmail.com>
2023-05-24 04:06:50 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints name from pointer
+ * @name: char string to add
+ * @f: function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
