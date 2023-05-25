91550a2b4d89ae890575ee354bd6f06853785c76
ntshewu <ntshewu@gmail.com>
2023-05-25 14:09:33 -0700

    Task2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,33 +3,34 @@
 #include <stdarg.h>
 
 /**
- * print_strings - prints strings, followed by new line
- * @separator: string to be printed between the strings
- * @n: number of strings passed to the function
+ * print_strings - Prints strings, followed by a new line.
+ * @separator: The string to be printed between strings.
+ * @n: The number of strings passed to the function.
+ * @...: number of strings to be printed.
  *
- * Return: null
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	char *sep, *ptr;
-	unsigned int i;
-	va_list list;
+	va_list strings;
+	char *str;
+	unsigned int index;
 
-	if (separator == NULL || *separator == 0)
-		sep = "";
-	else
-		sep = (char *) separator;
-	va_start(list, n);
+	va_start(strings, n);
 
-	if (n > 0)
-		printf("%s", va_arg(list, char *));
-	for (i = 1; i < n; i++)
+	for (index = 0; index < n; index++)
 	{
-		ptr = va_arg(list, char*);
-		if (ptr == NULL)
-			ptr = "(nil)";
-		printf("%s%s", sep, ptr);
+		str = va_arg(strings, char *);
+
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+
+		if (index != (n - 1) && separator != NULL)
+			printf("%s", separator);
 	}
+
 	printf("\n");
-	va_end(list);
+
+	va_end(strings);
 }
